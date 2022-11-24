44b6a4a163576ff3267c07c2b52a13877ccdeb54
Christian Rivero Arnez <70019894+DwijanX@users.noreply.github.com>
2022-11-24 09:31:56 -0400

    function to guess now ignores dots

--- src/password.js
+++ src/password.js
@@ -1,11 +1,25 @@
 let password="El password de mi cuenta es PASSWORD. Es un Password de poco cuidado"
 
+function deleteDotsOnSentence(password)
+{
+    let newPass=""
+    for(let index=0;index<password.length;index++)
+    {
+        if(password[index]==".")
+            newPass+=" "
+        else
+            newPass+=password[index]
+    }
+    return newPass
+}
 function guessPasswordWord(guess)
 {
     guess=guess.toLowerCase()
+    let passwordToGuess=password.toLowerCase()
+    passwordToGuess=deleteDotsOnSentence(passwordToGuess);
+    let wordsArray=passwordToGuess.split(" ")
     let timesGuessAppeared=0;
-    password=password.toLowerCase()
-    let wordsArray=password.split(" ")
+
     for(let wordIndex=0;wordIndex<=wordsArray.length;wordIndex++)
     {
         if(wordsArray[wordIndex]==guess)
--- src/password.spec.js
+++ src/password.spec.js
@@ -2,9 +2,12 @@ import {guessPasswordWord} from "./password"
 
 describe("Passwords", () => {
     it("Deberia adivinar password", () => {
-        expect(guessPasswordWord("password")).toEqual(2);
+        expect(guessPasswordWord("cuidado")).toEqual(1);
       });
     it("Deberia adivinar password sin importar si es mayuscula o minuscula", () => {
-    expect(guessPasswordWord("paSsWorD")).toEqual(2);
+    expect(guessPasswordWord("el")).toEqual(1);
     });
+    it("Deberia adivinar password sin importar .", () => {
+        expect(guessPasswordWord("paSsWorD")).toEqual(3);
+        });
 });
